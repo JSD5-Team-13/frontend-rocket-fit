9a5a9a056634844de623b8ee247710fb001f8d2f
krystalemblems <siwakorn.nd@gmail.com>
2023-10-05 11:33:15 +0700

    Added calendar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import Calendar from './component/Calandar/Calendar'
 import './App.css'
-import Mainpage from './component/Mainpage/Mainpage'
 
 function App() {
   return (
     <>
-    <Mainpage />
+    <Calendar />
     </>
   )
 }
