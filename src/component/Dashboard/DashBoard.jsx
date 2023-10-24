b2a3eb4b4774a5abc2c8b142cd59ca57c18be53e
kongcmk <whoiskong@gmail.com>
2023-10-24 09:49:23 +0700

    commit

--- src/component/Dashboard/DashBoard.jsx
+++ src/component/Dashboard/DashBoard.jsx
@@ -140,7 +140,7 @@ export const DashBoard = () => {
       <NavbarLoggedIn />
       <div
         id="dashboard"
-        className="max-w-screen-2xl mx-auto flex flex-row relative z-0"
+        className="relative z-0 flex flex-row mx-auto max-w-screen-2xl"
       >
         {/* SideInformation */}
         <div
@@ -162,7 +162,7 @@ export const DashBoard = () => {
             <strong>WEEKLY OVERVIEW</strong>
           </h3>
 
-          <div className=" lg:m-0 lg:p-0 m-5">
+          <div className="m-5  lg:m-0 lg:p-0">
             {/* card */}
             <div
               id="card_Sleep-BMI"
@@ -173,7 +173,7 @@ export const DashBoard = () => {
                   <div className="card-body">
                     <p className="text-[1.5rem]">4.25</p>
                     <h2 className="card-title text-[2rem]">Hours</h2>
-                    <div className="card-actions justify-end">
+                    <div className="justify-end card-actions">
                       <BiSolidMoon className="text-[50px]" />
                     </div>
                   </div>
@@ -188,7 +188,7 @@ export const DashBoard = () => {
                   <div className="card-body">
                     <p className="text-[1.5rem]">16.65</p>
                     <h2 className="card-title text-[2rem]">BMI</h2>
-                    <div className="flex  justify-end">
+                    <div className="flex justify-end">
                       <svg
                         width="50px"
                         height="50px"
@@ -234,7 +234,7 @@ export const DashBoard = () => {
             {/* chart */}
             <div
               id="data-chart"
-              className=" flex lg:flex-row lg:px-3 lg:justify-between flex-col justify-around "
+              className="flex flex-col justify-around  lg:flex-row lg:px-3 lg:justify-between"
             >
               {/* bar */}
               <div
