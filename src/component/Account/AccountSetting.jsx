49c07e716cf0e841f8b67fbc0756f5beebbf1835
kongcmk <whoiskong@gmail.com>
2023-10-24 23:22:30 +0700

    fixed responsive account page

--- src/component/Account/AccountSetting.jsx
+++ src/component/Account/AccountSetting.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
 import React, { useEffect, useState } from "react";
 import Layout from "../Layout.jsx";
 import Swal from "sweetalert2";
